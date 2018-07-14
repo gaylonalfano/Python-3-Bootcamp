5b688d9fc2998c5c48489fcc8e60fda339cc0999
Gaylon Alfano <gaylon.alfano@gmail.com>
2018-07-14 00:14:29 -0500

    Working with list comprehension now

--- list_comprehension.py
+++ list_comprehension.py
@@ -39,6 +39,8 @@ string_list = [str(num) for num in numbers]
 print(string_list)  # ['1', '2', '3', '4', '5']
 
 
+
+
 # List comprehension with conditional IF logic
 evens = [num for num in numbers if num % 2 == 0]
 odds = [num for num in numbers if num % 2 != 0]
