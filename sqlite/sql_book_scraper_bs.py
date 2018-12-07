84aac8230af29e476a4649ea5f799fae14b6d74a
gaylonalfano <gaylon.alfano@gmail.com>
2018-12-08 07:48:23 +0800

    Completed spaghetti code version of scraping book title, price, and rating. Next is to refactor by creating some functions instead and consolidating all data into a list so we can load into SQLite.

--- sqlite/sql_book_scraper_bs.py
+++ sqlite/sql_book_scraper_bs.py
@@ -9,6 +9,25 @@ response = requests.get("http://books.toscrape.com/catalogue/category/books/hist
 soup = BeautifulSoup(response.text, "html.parser")
 # Extract the data we want (title, price, rating)
 books = soup.find_all("article")
-print(books)
+for book in books:
+    # Get the title:
+    # title = book.find("h3").find("a")["title"]
+    # # Get the price:
+    # price = book.select(".price_color")[0].get_text()  # Â£54.23
+    # # Replace/get rid of the currency symbols and convert to float:
+    # price = float(price.replace("Â","").replace("£",""))
+    # Get the review. It's hard because it's a class within a class
+    # Create a dict to help convert str rating to int:
+    ratings = {"Five": 5, "Four": 4, "Three": 3, "Two": 2, "One": 1, "Zero": 0}
+    # If you don't add [0] after .select()[0], then you get an error
+    # since it's trying to get_text() on a LIST instead of the element.
+    paragraph = book.select(".star-rating")[0]
+    # Retrieve the STR rating ('Five', 'Four', etc.)
+    str_rating = paragraph.get_attribute_list("class")[1]
+    int_rating = ratings[str_rating]
+    print(int_rating) # 5, 1, 1, ...
+    #print(title, price)
+
+# print(books)
 
 # Save dave to the database
