b069e835b95c4bd16644536060478513bb5a462d
gaylonalfano <gaylon.alfano@gmail.com>
2018-12-08 10:55:33 +0800

    Refactored book scraper code. Next step is to store into the database.

--- sqlite/sql_book_scraper_bs.py
+++ sqlite/sql_book_scraper_bs.py
@@ -3,31 +3,62 @@ import sqlite3
 import requests
 from bs4 import BeautifulSoup
 
-# Request the URL
-response = requests.get("http://books.toscrape.com/catalogue/category/books/history_32/index.html")
-# Initialize BeautifulSoup
-soup = BeautifulSoup(response.text, "html.parser")
-# Extract the data we want (title, price, rating)
-books = soup.find_all("article")
-for book in books:
-    # Get the title:
-    # title = book.find("h3").find("a")["title"]
-    # # Get the price:
-    # price = book.select(".price_color")[0].get_text()  # Â£54.23
-    # # Replace/get rid of the currency symbols and convert to float:
-    # price = float(price.replace("Â","").replace("£",""))
-    # Get the review. It's hard because it's a class within a class
-    # Create a dict to help convert str rating to int:
+
+def scrape_books(url):
+    '''Scrape the url for books'''
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    books = soup.find_all("article")
+    all_books = []
+    for book in books:
+        # Store in tuple
+        book_data = (get_title(book), get_price(book), get_rating(book))
+        # Store to all_books list:
+        all_books.append(book_data)
+    print(all_books)
+
+def get_title(book):
+    return book.find("h3").find("a")["title"]
+
+def get_price(book):
+    price = book.select(".price_color")[0].get_text()
+    return float(price.replace("Â","").replace("£",""))
+
+def get_rating(book):
     ratings = {"Five": 5, "Four": 4, "Three": 3, "Two": 2, "One": 1, "Zero": 0}
-    # If you don't add [0] after .select()[0], then you get an error
-    # since it's trying to get_text() on a LIST instead of the element.
     paragraph = book.select(".star-rating")[0]
-    # Retrieve the STR rating ('Five', 'Four', etc.)
     str_rating = paragraph.get_attribute_list("class")[1]
     int_rating = ratings[str_rating]
-    print(int_rating) # 5, 1, 1, ...
-    #print(title, price)
+    return int_rating
 
-# print(books)
+# Now we call the functions to scrape and store:
+scrape_books("http://books.toscrape.com/catalogue/category/books/history_32/index.html")
+
+# BELOW IS SPAGHETTI CODE. ABOVE IS REFACTORED VERSION.
+
+# # Request the URL
+# response = requests.get("http://books.toscrape.com/catalogue/category/books/history_32/index.html")
+# # Initialize BeautifulSoup
+# soup = BeautifulSoup(response.text, "html.parser")
+# # Extract the data we want (title, price, rating)
+# books = soup.find_all("article")
+# for book in books:
+#     # Get the title:
+#     title = book.find("h3").find("a")["title"]
+#     # Get the price:
+#     price = book.select(".price_color")[0].get_text()  # Â£54.23
+#     # Replace/get rid of the currency symbols and convert to float:
+#     price = float(price.replace("Â","").replace("£",""))
+#     # Get the review. It's hard because it's a class within a class
+#     # Create a dict to help convert str rating to int:
+#     ratings = {"Five": 5, "Four": 4, "Three": 3, "Two": 2, "One": 1, "Zero": 0}
+#     # If you don't add [0] after .select()[0], then you get an error
+#     # since it's trying to get_text() on a LIST instead of the element.
+#     paragraph = book.select(".star-rating")[0]
+#     # Retrieve the STR rating ('Five', 'Four', etc.)
+#     str_rating = paragraph.get_attribute_list("class")[1]
+#     int_rating = ratings[str_rating]
+#     print(int_rating) # 5, 1, 1, ...
+#     #print(title, price)
 
 # Save dave to the database
